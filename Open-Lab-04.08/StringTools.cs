50441d0b5abd5e184ecc9acf75855618c9b92470
einnarth <113421210+einnarth@users.noreply.github.com>
2022-10-13 09:06:05 +0200

    Update StringTools.cs

--- Open-Lab-04.08/StringTools.cs
+++ Open-Lab-04.08/StringTools.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 
 namespace Open_Lab_04._08
 {
@@ -6,7 +7,15 @@ namespace Open_Lab_04._08
     {
         public string[] IsFourLetters(string[] strings)
         {
-            throw new NotImplementedException();
+            List<string> letters = new List<string>();
+            foreach (string word  in strings)
+            {
+                if (word.Length == 4 )
+                {
+                    letters.Add(word);
+                }
+            }
+            return  letters.ToArray();
         }
     }
 }
